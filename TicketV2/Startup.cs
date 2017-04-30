1aacf205620f64e1bdebbe227ff3057dd9c1c81a
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2017-04-30 13:24:30 +0300

    Add project files.

---
+++ TicketV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketV2.Startup))]
+namespace TicketV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
